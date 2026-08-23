14cbeb44d485d440ab840a21fd87b5e1e43ace3f
flv <208173943+flv-byte@users.noreply.github.com>
2026-08-23 03:48:36 +0300

    Small building performance improvement

---
+++ src/pch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#include <windows.h>
+#include <intrin.h>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <thread>
+#include <vector>
+
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
